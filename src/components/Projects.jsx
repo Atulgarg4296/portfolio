325495f3f6ed6f231495dd56d5fba5b3a53f0c95
Atul Garg <atulgarg4296@gmail.com>
2025-02-18 00:26:44 +0530

    Project margin

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     
     <div id="projects" className="mt-20 md:mt-0">
-      <h1 className="text-white font-sans text-4xl font-extralight text-center mb-4">PROJECTS</h1>
+      <h1 className="text-white font-sans text-4xl font-extralight text-center mb-4 mt-8 md:mt-0">PROJECTS</h1>
     
     <div className=" min-h-96 md:mb-20 mb-10 md:flex md:justify-around grid justify-center gap-y-8">
         <ProjectCard title={"Netflix Clone"} imageSrc={white_netflix} projectLink={netflix_link}/>
