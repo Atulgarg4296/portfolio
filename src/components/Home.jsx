d2ba5c1dce0e50d7e70cd1d5e0a3317dc808ab5c
Atul Garg <atulgarg4296@gmail.com>
2025-02-17 23:28:40 +0530

    Responsive navbar and Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
 import Atul_picture from "../utils/Atul_picture.jpg"
 const Home = () => {
   return (
-    <div id="home" className="text-white flex" >
-        <div className=" w-1/2 h-screen">
-            <h1 className="text-white text-6xl font-serif pl-64 pt-64">ATUL GARG</h1>
-            {/* <p className="pl-56 ">About section</p> */}
-            <p className="pl-64 pb-2">You can download my resume from here!</p>
-            <a href="./ATUL_GARG.pdf" download="Atul's_Resume" className="ml-64 px-4  rounded py-1 gap-y-2  bg-zinc-800">Resume</a>
-        </div>
+    <div id="home" className="text-white flex md:h-screen h-screen" >
         <div className=" w-1/2">
-            <img src={Atul_picture} alt="Atul_Garg_picture" className="h-64 rounded-2xl w-80 mt-48 ml-16"></img>
+            <h1 className="text-white text-6xl font-serif md:pl-64 md:pt-64 pl-32 pt-26  ">ATUL GARG</h1>
+            {/* <p className="pl-56 ">About section</p> */}
+            <p className="md:pl-64 md:pb-2 w-screen pl-18 mb-2 md:mb-0 ">You can download my resume from here!</p>
+            <a href="./ATUL_GARG.pdf" download="Atul's_Resume" className="md:ml-64 ml-40 px-4  rounded py-1 gap-y-2   bg-zinc-800">Resume</a>
+        </div>
+        <div className=" w-1/2 hidden md:block">
+            <img src={Atul_picture} alt="Atul_Garg_picture" className="h-64 rounded-2xl w-80 mt-48 ml-16 "></img>
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-24 text-white  w-full bg-black" >
         <div className="">
-            <h1 className="text-4xl font-serif px-6">Portfolio</h1>
+            <h1 className="text-4xl font-serif px-6 mx-28 md:mx-2">Portfolio</h1>
         </div>
         <ul className=" flex gap-x-6  px-6 text-xl font-semibold ">
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#skills">SKILLS</a></li>
-            <li><a href="#projects">PROJECTS</a></li>
-            <li><a href="#footer">CONTACT</a></li>
+            <li className="hidden md:block"><a href="#home">HOME</a></li>
+            <li className="hidden md:block"><a href="#skills">SKILLS</a></li>
+            <li className="hidden md:block"><a href="#projects">PROJECTS</a></li>
+            <li className="hidden md:block"><a href="#footer">CONTACT</a></li>
         </ul>
 
     </div>
